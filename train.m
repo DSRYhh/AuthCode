c46db0a7977f450283c33c71aa8be11915ab6af3
黄煜恒 <hyhlryf@gmail.com>
2017-05-30 12:57:43 +0800

    Select 30% to calculate accuary

--- train.m
+++ train.m
@@ -18,8 +18,12 @@ initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
 
 %% Load data
 load('data.mat');
-dataLength = size(data,1);
-X = X(1:dataLength,:);
+dataLength = size(data,1); %data: Y, X: image
+dataVeriLength = int16(dataLength * 0.3);
+XVeri = X(1:dataVeriLength);
+YVeri = data(1:dataVeriLength);
+X = X(dataVeriLength + 1:end,:);
+data = data(dataVeriLength + 1:end,:);
 
 
 %% Train NN
@@ -45,6 +49,6 @@ save('weights.mat','Theta1','Theta2');
              
 %% Prediction
 
-pred = predict(Theta1, Theta2, X);
+pred = predict(Theta1, Theta2, XVeri);
 
-fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == data)) * 100);
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == YVeri)) * 100);
